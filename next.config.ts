4dae06bdf5b980b4718be7b3096d7b390be9ace9
polarfreez <rafaelbraun5@hotmail.com>
2025-07-14 04:24:05 -0300

    Updated dependencies and configuration for Next.js PWA

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,20 @@
-import type { NextConfig } from 'next';
-import withPWA from 'next-pwa';
+/** @type {import('next').NextConfig} */
 
-const nextConfig: NextConfig = {
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === 'development',
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
+const nextConfig = {
   output: 'export',
   basePath: process.env.PAGES_BASE_PATH,
 };
 
-export default withPWA({
-  ...nextConfig,
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
-  },
-});
+module.exports = withPWA(nextConfig);
