eed49e1415acf8f5bd871d6f291425683b7f23aa
polarfreez <rafaelbraun5@hotmail.com>
2025-07-14 04:07:25 -0300

    Refactor PWA configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,16 @@
 import type {NextConfig} from 'next';
 import withPWA from 'next-pwa';
 
+const pwaConfig = {
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+};
+
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: process.env.PAGES_BASE_PATH,
 };
 
-export default withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-})(nextConfig);
+export default withPWA(pwaConfig)(nextConfig);
